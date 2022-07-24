c7f523dfd50b97ed6373e8cd4b42b9b5db8a340b
Jonggu Lee <jg.jake.lee@gmail.com>
2022-07-24 19:26:33 +0900

    Update dev env

--- dev/main.tf
+++ dev/main.tf
@@ -7,14 +7,21 @@ module "vpc" {
   name   = "dev"
   cidr   = "172.17.0.0/16"
 
-  azs              = ["ap-northeast-2a", "ap-northeast-2c"]
-  public_subnets   = ["172.17.1.0/24", "172.17.3.0/24"]
-  private_subnets  = ["172.17.101.0/24", "172.17.103.0/24"]
+  azs                = ["ap-northeast-2a", "ap-northeast-2c"]
+  public_subnets     = ["172.17.1.0/24", "172.17.3.0/24"]
+  private_subnets    = ["172.17.101.0/24", "172.17.103.0/24"]
   restricted_subnets = ["172.17.201.0/24", "172.17.203.0/24"]
 
   # VPC module이 생성하는 모든 리소스에 기본으로 입력될 Tag를 정의한다.
   tags = {
     "TerraformManaged" = "true",
-    "Environment" = "dev"
+    "Environment"      = "dev"
   }
+}
+
+module "key_pair" {
+  source = "../modules/key-pair"
+  # key_name           = "dev-marketplace-front-key"
+  key_name           = ["dev-marketplace-front-key", "dev-marketplace-api-key", "test"]
+  create_private_key = true
 }
--- dev/output.tf
+++ dev/output.tf
@@ -5,18 +5,24 @@ output "vpc" {
 }
 
 output "public_subnet_cidr" {
-    value   = module.vpc.public_subnets_cidr
-    description = "The name of vpc"
+  value       = module.vpc.public_subnets_cidr
+  description = "The name of vpc"
 }
 
 output "private_subnet_cidr" {
-    value   = module.vpc.private_subnets_cidr
+  value = module.vpc.private_subnets_cidr
 }
 
 output "restricted_subnet_cidr" {
-    value   = module.vpc.restricted_subnets_cidr
+  value = module.vpc.restricted_subnets_cidr
 }
 
 output "nat_public_ips" {
-    value   = module.vpc.nat_public_ips
+  value = module.vpc.nat_public_ips
+}
+
+# Key Pair
+output "key_pairs_private_key_pem" {
+  value = module.key_pair.private_key_pem
+  sensitive   = true
 }
