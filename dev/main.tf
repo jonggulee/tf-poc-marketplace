65c20bc59bac9e9ec8b41b29719a0c1f49555724
Jonggu Lee <jg.jake.lee@gmail.com>
2022-07-27 20:01:02 +0900

    Edit key_path

--- dev/main.tf
+++ dev/main.tf
@@ -12,7 +12,6 @@ module "vpc" {
   private_subnets    = ["172.17.101.0/24", "172.17.103.0/24"]
   restricted_subnets = ["172.17.201.0/24", "172.17.203.0/24"]
 
-  # VPC module이 생성하는 모든 리소스에 기본으로 입력될 Tag를 정의한다.
   tags = {
     "TerraformManaged" = "true",
     "Environment"      = "dev"
@@ -21,8 +20,7 @@ module "vpc" {
 
 module "key_pair" {
   source = "../modules/key-pair"
-  # key_name           = "dev-marketplace-front-key"
   key_name           = ["dev-marketplace-front-key", "dev-marketplace-api-key"]
-  key_path           = ["./key/dev-marketplace-front-key.pem", "./key/dev-marketplace-api-key.pem"]
+  key_path           = ["./key/dev-marketplace-front-key.pub", "./key/dev-marketplace-api-key.pub"]
   create_private_key = true
 }
